feb811aff13561d7a3705906e8d4d79193ae28ef
Shahbaz Ansari <ansarishahbaz96@gmail.com>
2023-05-31 23:49:46 +0530

    Added SideBar component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import { Box, Typography, Stack } from "@mui/material";
+import SideBar from "./SideBar";
 
 const Feed = () => {
   return (
-    <div>
-      Feed
-    </div>
-  )
-}
+    <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
+      <Box
+        sx={{
+          height: { sx: "auto", md: "92vh" },
+          borderRight: "1px solid #3d3d3d",
+          padding: { sx: 0, md: 2 },
+        }}
+      >
+        <SideBar />
+        <Typography className="copyright" variant="body2" sx= {{mt: 1.5, color: "#fff"}}>
+          Copyright 2022 Shahbaz Ansari
+        </Typography>
+      </Box>
+    </Stack>
+  );
+};
 
-export default Feed
+export default Feed;
