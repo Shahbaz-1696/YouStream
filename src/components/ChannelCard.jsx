05ad581eb77bbfdb168d892317ef22c3fd377cb3
Shahbaz Ansari <ansarishahbaz96@gmail.com>
2023-06-06 11:50:34 +0530

    Completed VideoCard and ChannelCard components

--- src/components/ChannelCard.jsx
+++ src/components/ChannelCard.jsx
@@ -1,11 +1,63 @@
-import React from 'react'
+import React from "react";
+import { Box, CardContent, Typography, CardMedia } from "@mui/material";
+import { CheckCircle } from "@mui/icons-material";
+import { Link } from "react-router-dom";
+import { demoProfilePicture } from "../utils/constants";
 
-const ChannelCard = () => {
+const ChannelCard = ({ channelDetail }) => {
   return (
-    <div>
-      ChannelCard
-    </div>
-  )
-}
+    <Box
+      sx={{
+        borderRadius: '20px',
+        boxShadow: 'none',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        margin: 'auto',
+        height: '326px',
+        width: { xs: '356px', md: '320px'}
+      }}
+    >
+      <Link to={`/channel/${channelDetail?.id?.channelId}`}>
+        <CardContent
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            textAlign: "center",
+            flexDirection: "column",
+            color: "#fff",
+          }}
+        >
+          <CardMedia
+            image={
+              channelDetail?.snippet?.thumbnails?.high?.url ||
+              demoProfilePicture
+            }
+            alt={channelDetail?.snippet?.title}
+            sx={{
+              borderRadius: "50%",
+              height: "180px",
+              width: "180px",
+              mb: 2,
+              border: "1px solid #e3e3e3",
+            }}
+          />
+          <Typography variant="h6">
+            {channelDetail?.snippet?.title}
+            <CheckCircle sx={{ fontSize: 15, color: "gray", ml: "5px" }} />
+          </Typography>
+          {channelDetail?.statistics?.subscriberCount && (
+            <Typography>
+              {parseInt(
+                channelDetail?.statistics?.subscriberCount
+              ).toLocaleString()}{" "}
+              Subscribers
+            </Typography>
+          )}
+        </CardContent>
+      </Link>
+    </Box>
+  );
+};
 
-export default ChannelCard
+export default ChannelCard;
