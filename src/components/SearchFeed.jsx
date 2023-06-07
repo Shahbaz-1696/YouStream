ce021558b727971818ea629fa8a4371392d90802
Shahbaz Ansari <ansarishahbaz96@gmail.com>
2023-06-07 11:27:21 +0530

    Completed SearchFeed and SearchBar components

--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,11 +1,28 @@
-import React from 'react'
+import { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import { Videos } from "./";
+import { fetchFromApi } from "../utils/fetchFromApi";
 
 const SearchFeed = () => {
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromApi(`search?part=snippet&q=${searchTerm}`).then((data) =>
+      setVideos(data.items)
+    );
+  }, [searchTerm]);
+
   return (
-    <div>
-      SearchFeed
-    </div>
-  )
-}
+    <Box p={2} sx={{ overflowY: "auto", flex: 2, height: "90vh" }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results for:{" "}
+        <span style={{ color: "#FC1503" }}>{searchTerm}</span> videos
+      </Typography>
+      <Videos videos={videos} />
+    </Box>
+  );
+};
 
 export default SearchFeed;
